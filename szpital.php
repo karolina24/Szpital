1d47b6cc19ca70100e476e3c0edfa564da0a8990
AsfaltowyLekarz <148557173+AsfaltowyLekarz@users.noreply.github.com>
2023-11-10 11:46:55 +0100

    Add files via upload

---
+++ szpital.php
@@ -0,0 +1,30 @@
+<?php
+class szpital{
+    private $polaczenie;
+    private $komunikat='';
+    private function nawiazPolaczenie(){
+		$this->polaczenie = new mysqli('localhost','root','','szpital');
+        mysqli_set_charset($this->polaczenie,'utf8');
+    }
+    private function wyswietlTabele(){
+        $zapytanie='SELECT * FROM lekarze ORDER BY Id_lekarza';
+        $wynik_zapytania=$this->polaczenie->query($zapytanie);
+        $tmp = '<h2>Zawartość tabeli lekarze</h2><table>';
+		 $tmp .= "<tr><th>Nr</th><th>Nazwisko</th><th>Imię</th><th>Specjalizacja</th><th>Oddział</th></tr>";
+		 $i=1;
+         while($obiekt=mysqli_fetch_object($wynik_zapytania)){
+            $tmp .= "<tr><td>$i</td><td>$obiekt->Imię</td><td>$obiekt->Nazwisko</td><td>$obiekt->Specjalizacja</td><td>$obiekt->Oddział</td></tr>";
+             $i++;
+        }
+        return $tmp .'</table>';
+    }
+    function wyswietlStrone(){
+        $tmp = $this->wyswietlTabele();
+        $tmp .= $this->komunikat;
+        return $tmp;
+    }
+    function __construct(){
+        $this->nawiazPolaczenie();
+    }
+}
+?>
